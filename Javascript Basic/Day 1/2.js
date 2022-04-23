926553d643a07d7925269f3c60772981f9471ba6
chtrli <chtrl2022@gmail.com>
2022-04-23 12:34:16 +0530

    'day-2'

---
+++ Javascript Basic/Day 1/2.js
@@ -0,0 +1,7 @@
+// Write a JavaScript program to print the contents of the current window. 
+//To print contents of a page use window.print();
+
+//Write a JavaScript program to get the current date.
+
+let dt = new Date();
+console.log(dt);
