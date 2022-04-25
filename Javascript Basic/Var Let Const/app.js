2745044989bd001f2694144676964b5057b8d633
chtrli <chtrl2022@gmail.com>
2022-04-25 13:05:02 +0530

    var-let-const

---
+++ Javascript Basic/Var Let Const/app.js
@@ -0,0 +1,18 @@
+//Const Var and Let
+
+//Function scope inside the function
+
+//Block scope inside the block eg. For if etc
+
+if(true){
+    var varV = "This is true";
+}
+
+console.log(varV);
+
+if(true){
+    let letV = "This is true";
+
+}
+
+console.log(letV);
